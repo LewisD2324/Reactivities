12119dd472efb1dd63224d4f0224ed0dff541204
LewisD23 <41574278+LewisD23@users.noreply.github.com>
2019-10-14 12:14:54 +0100

    Initial Commit

---
+++ Reactivities.Persistance/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Reactivities.Domain;
+
+namespace Reactivities.Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                new Value {Id = 1, Name = "Value 101"},
+                new Value {Id = 2, Name = "Value 102"},
+                new Value {Id = 3, Name = "Value 103"}
+            );
+        }
+    }
+}
